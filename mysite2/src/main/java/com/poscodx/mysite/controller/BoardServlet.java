3f266912c43de97c90ca77aca1349cf2552be23c
kimgip <gippeum0102@gmail.com>
2024-05-31 08:51:04 +0900

    (model2 최종 과제) mysite2 : board servlet completed - Action 계획

--- mysite2/src/main/java/com/poscodx/mysite/controller/BoardServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/BoardServlet.java
@@ -1,12 +1,29 @@
 package com.poscodx.mysite.controller;
 
+import java.util.Map;
+
+import com.poscodx.mysite.controller.action.board.BoardAction;
+import com.poscodx.mysite.controller.action.board.DeleteAction;
+import com.poscodx.mysite.controller.action.board.ModifyAction;
+import com.poscodx.mysite.controller.action.board.ModifyFormAction;
+import com.poscodx.mysite.controller.action.board.ViewFormAction;
+import com.poscodx.mysite.controller.action.board.WriteAction;
+import com.poscodx.mysite.controller.action.board.WriteFormAction;
 
 public class BoardServlet extends ActionServlet {
 	private static final long serialVersionUID = 1L;
+	Map<String, Action> mapAction = Map.of(
+			"writeform", new WriteFormAction(),
+			"write", new WriteAction(),
+			"delete", new DeleteAction(),
+			"viewform", new ViewFormAction(),
+			"modifyform", new ModifyFormAction(),
+			"modify", new ModifyAction()
+		);
 
 	@Override
 	protected Action getAction(String actionName) {
-		return null;
+		return mapAction.getOrDefault(actionName, new BoardAction());
 	}
     
 
