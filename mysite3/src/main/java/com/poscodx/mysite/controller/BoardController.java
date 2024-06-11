ba2e8ff17ef9a6514104d0ed0046279b2fa0e1d4
kimgip <gippeum0102@gmail.com>
2024-06-11 09:46:24 +0900

    import 삭제

--- mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
@@ -2,12 +2,9 @@ package com.poscodx.mysite.controller;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
