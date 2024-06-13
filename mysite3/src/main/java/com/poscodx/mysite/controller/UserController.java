f49f9a1dc68f22b7692d1ffc2e59f9acf8bd8e4d
kimgip <gippeum0102@gmail.com>
2024-06-13 11:30:51 +0900

    @ModelAttribute : model.addAttribute

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,9 +28,9 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@Valid UserVo vo, BindingResult result, Model model) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
 		if(result.hasErrors()) {
-			model.addAttribute("userVo", vo);
+//			model.addAttribute("userVo", vo);
 			
 //			List<ObjectError> list = result.getAllErrors();
 //			for(ObjectError error:list) {
