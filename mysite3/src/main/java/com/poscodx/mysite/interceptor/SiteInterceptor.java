225571245a667d1e85e6750ddfca2818db705908
kimgip <gippeum0102@gmail.com>
2024-06-17 14:07:54 +0900

    (mysite3) 다국어지원[1]default:AcceptHeaderLocaleReader

--- mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
+++ mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
@@ -3,17 +3,20 @@ package com.poscodx.mysite.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.ognl.enhance.LocalReference;
+import org.apache.tomcat.util.descriptor.LocalResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
 
 import com.poscodx.mysite.service.SiteService;
 import com.poscodx.mysite.vo.SiteVo;
 
 public class SiteInterceptor implements HandlerInterceptor {
+	@Autowired
+	private LocaleResolver localeResolver;
+	@Autowired
 	private SiteService siteService;
-	
-	public SiteInterceptor(SiteService siteService) {
-		this.siteService = siteService;
-	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -23,6 +26,9 @@ public class SiteInterceptor implements HandlerInterceptor {
 		if(siteVo == null) {
 			request.getServletContext().setAttribute("siteVo", siteService.getSite());
 		}
+		
+		// Locale
+		System.out.println("resolver-locale: "+localeResolver.resolveLocale(request).getLanguage());
 		return true;
 	}
 
